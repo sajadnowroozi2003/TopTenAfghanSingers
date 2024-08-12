0d31196f3b7b70cc82bd09b7a2adfe860b9812f6
sajad <sajad.nowroozi5442@gmail.com>
2024-08-12 17:47:17 +0430

    finish splash screen and create home screen

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Directionality(
+          textDirection: TextDirection.rtl,
+          child: HomeScreen())));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+                child: SvgPicture.asset(
+                  'assets/images/logo.svg',
+                  height: 300,
+                )),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'ده خواننده برتر افغان',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: 'SplashFont',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.teal),
+                  ),
+                  Text('v 0.1.1'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
