f58971cdcc9515c313a4f69d09c071ffab757986
sajad <sajad.nowroozi5442@gmail.com>
2024-08-12 18:28:46 +0430

    Starting Create PopupMenuBottom on Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       locale: Locale('fa', ''),
       localeResolutionCallback: (locale, supportedLocales) {
         return locale;
