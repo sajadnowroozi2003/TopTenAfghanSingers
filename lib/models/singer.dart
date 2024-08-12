ad936ba13f2ae973c670468fc4e973ae6dc31b66
sajad <sajad.nowroozi5442@gmail.com>
2024-08-12 17:49:18 +0430

    create class singer model

---
+++ lib/models/singer.dart
@@ -0,0 +1,8 @@
+class Singer {
+  List<String> musicsPath = [];
+  String? name;
+  String? description;
+  String? imageURL;
+
+  Singer(this.musicsPath, this.name, this.description, this.imageURL) {}
+}
