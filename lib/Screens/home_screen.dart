a54765f06e22fbe176b5f6fd717df36fc6323524
sajad <sajad.nowroozi5442@gmail.com>
2024-08-15 05:32:40 +0430

    add music in application

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:top_ten_afghan_singer/Screens/singer_details.dart';
-
 import '../models/popup_menu_item.dart';
 import '../models/singer.dart';
 
@@ -25,12 +24,22 @@ class _HomeScreenState extends State<HomeScreen> {
       'AhmadZaher_8',
       'AhmadZaher_9',
       'AhmadZaher_10',
-
     ],
         'احمد ظاهر',
         'احمد ظاهر (۲۴ جوزا یا خرداد ۱۳۲۵ دیده به جهان گشود– ۲۴ جوزا یا خرداد ۱۳۵۸ دیده از جهان فرو بست.) آواز خوان برجستهٔ افغانستان[۱] در دهه‌های ۵۰ و ۶۰ خورشیدی بود.[۲] از احمد ظاهر با عنوان《 بهترین و محبوب‌ترین خوانندهٔ تاریخ افغانستان 》یاد می‌شود.[۳] وی در عمر کوتاه خود بیشتر از سی و سه آلبوم منتشر کرد.و در مجموع تعداد آهنگ های احمد ظاهر به ۴۳۰ آهنگ می‌رسد. احمدظاهر، به زبان های دیگری هم مانند: انگلیسی، هندی و پشتو آهنگ می‌خواند. او در سالروز تولد ۳۳ سالگی‌اش در سانحه‌ای «مشکوک»[۴] در تونل سالنگ، درگذشت.[۵] همچنین وی را در افغانستان «الماس شرق» نامیده اند و در خارج از مرزهای افغانستان (در اروپا و آمریکا) مشهور به الویس پریسلی افغانستان بود .',
         'assets/images/singers_images/AhmadZaher.jpg'),
-    Singer([],
+    Singer([
+      'AhmadWali_1',
+      'AhmadWali_2',
+      'AhmadWali_3',
+      'AhmadWali_4',
+      'AhmadWali_5',
+      'AhmadWali_6',
+      'AhmadWali_7',
+      'AhmadWali_8',
+      'AhmadWali_9',
+      'AhmadWali_10',
+    ],
         'احمد ولی',
         'احمد ولی خواننده‌ای پاپ و غزل اهل افغانستان است. وی از دههٔ ۱۹۷۰ کار خود را آغاز کرد. قبل از اینکه مجبور به فرار به دلیل تحولات سیاسی در افغانستان شود. اولاً در آلمان به کار خود ادامه داد و در سراسر اروپا و آمریکای شمالی اجرا کرد. سپس به کشور آمریکا هجرت نمود.',
         'assets/images/singers_images/AhmadWali.jpg'),
@@ -38,7 +47,20 @@ class _HomeScreenState extends State<HomeScreen> {
         'وحید قاسمی',
         'وحید قاسمی آموزش‌های نخستین، متوسط و برتر را در مکتب سردار جانخان، لیسه نادریه و دانشکده ادبیات دانشگاه کابل فراگرفته‌است. سپس به موسیقی رو آورد و آموزش بنیان‌های موسیقی شرقی را از استاد یوسف قاسمی و استاد یعقوب قاسمی فراگرفت و اساسات موسیقی غربی را از استاد سلیم سرمست وببرک وسا آموخت. در سال ۱۹۷۷ گروه موسیقی "شاین " را ایجاد کرد که در زمینه موسیقی پاپ فعالیت دارد. ',
         'assets/images/singers_images/wahidGasemi.jpg'),
-    Singer([],
+    Singer([
+      'FarhadDrya_1',
+      'FarhadDrya_2',
+      'FarhadDrya_3',
+      'FarhadDrya_4',
+      'FarhadDrya_5',
+      'FarhadDrya_6',
+      'FarhadDrya_7',
+      'FarhadDrya_8',
+      'FarhadDrya_9',
+      'FarhadDrya_10',
+      'FarhadDrya_11',
+      'FarhadDrya_12',
+    ],
         'فرهاد دریا',
         'فرهاد ناشر با نام هنری فرهاد دریا (زاده ۲۲ سپتامبر ۱۹۶۲ در کابل، افغانستان) موسیقی‌دان، خواننده، بازیگر و فعال حقوق بشر اهل افغانستان و تأثیرگذارترین هنرمند افغانستان از دهه ۱۹۸۰ تا اکنون می‌باشد.[۱] او به عنوان یکی از برترین موسیقی‌دانان افغانستان نه تنها توسط موسیقی‌اش بلکه توسط فعالیت‌های انسان دوستانه و وطن‌دوستانه‌اش نزد مردم افغانستان شناخته می‌شود و از سال ۲۰۰۶ تاکنون سفیر صلح سازمان ملل متحد در افغانستان می‌باشد. طرفداران او شامل مناطق آسیای مرکزی، پاکستان، تاجیکستان، ازبکستان و در غرب بین افغانستانی‌ها و فارسی‌زبان‌ها هستند. دریا به زبان‌های گوناگون شامل فارسی، اوزبیکی، انگلیسی، پشتو و اردو ترانه می‌خواند.[۲] او تاکنون چندین کنسرت را در افغانستان، تاجیکستان، ایالات متحده، امارات متحده عربی و آلمان برگزار نموده‌است. کنسرت‌های او در افغانستان همیشه رایگان و بدون سود اجرا می‌شوند.',
         'assets/images/singers_images/Farhad Darya.jpg'),
@@ -51,31 +73,66 @@ class _HomeScreenState extends State<HomeScreen> {
         'داوود سرخوش (زادهٔ ۲۶ آوریل ۱۹۷۱ در ارزگان) خواننده، ترانه‌سرا و آهنگساز اهل افغانستان است. سرخوش بیشتر آهنگ‌های آلبوم‌هایش را خودش ساخته و اجرا می‌کند. ترانه‌ها بیشتر از سروده‌های وی هستند، اما از آثار شاعران معاصر افغانستان و ایران نیز بهره می‌برد.',
         'assets/images/singers_images/dawoodSarkhosh.jpg'),
     Singer([],
-        'خانم هنگامه',
-        'هنگامه در ۳ نوامبر ۱۹۶۱ در شهر کابل، افغانستان در خانواده‌ای هنردوست به دنیا آمد. پدرش غلام یحیی شورانگیز در هندوستان در رشته سینما تحصیل کرده‌است، مادرش صابره شورانگیز، زنی صاحب ذوق است که در مؤسسه هنرهای زیبا، افغان فلم و در رادیو و تلویزیون افغانستان کار کرده‌است. هنگامه یک برادر و چهار خواهر دارد که از این جمع، فقط خواهرش فرشته سما، به موسیقی روی آورده‌است. هنگامه در سال ۱۹۸۱ کابل را به مقصد پاکستان ترک کرد. هنگامه در سال ۱۹۸۵ در آلمان با احمد ولی ازدواج نمود ',
-        'assets/images/singers_images/hangama.jpg'),
+        'امیرجان صبوری',
+        'صبوری از شانزده سالگی به هنر آوازخوانی روی آورد و برای اولین بار جشنواره آهنگ‌های ملی و محلی در کابل آهنگی به نام دختر هری را خواند که با استقبال زیادی روبه‌رو شد و مقام دوم را به دست آورد و وی پس از آن این آهنگ را در رادیو افغانستان ضبط کرد.[۳]صبوری در سال ۱۳۶۰ ارکستری موسیقی به نام گل سرخ تشکیل داد که در آن هنرمندانی چون وجیهه و فرید رستگار، حفیظ وصال، محمود کامن، کبیر متین، شریف ساحل، نعیم ودیگران با او بودند.امیرجان صبوری در خوانندگی خود وقفه‌ای طولانی داشت اما در سال ۲۰۰۵ با آلبومی به نام «مستند به نام خواب طلایی ساخته شده‌است.[۴]صبوری پسر خاله خواننده تازه تواب آرش است که ساکن سوئد می‌باشد و صبوری به او برای تنظیم آلبوم چای و چلم کمک کرده‌است.[۵]',
+        'assets/images/singers_images/amirjanSabory.jpg'),
     Singer([],
         'قیس الفت',
         'قیس الفت یکی از خوانندگان جوان و محبوب افغان است که با صدای گرم و آثار خود توانسته جایگاه ویژه‌ای در موسیقی افغانستان پیدا کند. او بیشتر به خاطر ترانه‌های عاشقانه و اجتماعی‌اش شناخته می‌شود. قیس الفت در سبک‌های مختلف موسیقی، از جمله پاپ و فولکلور، فعالیت می‌کند و توانسته توجه زیادی را به خود جلب کند.',
         'assets/images/singers_images/qaiseOlfate.jpg'),
-    Singer([],
+    Singer([
+      'AryanaSaied_17',
+      'AryanaSaied_16',
+      'AryanaSaied_15',
+      'AryanaSaied_13',
+      'AryanaSaied_14',
+      'AryanaSaied_12',
+      'AryanaSaied_11',
+      'AryanaSaied_10',
+      'AryanaSaied_9',
+      'AryanaSaied_8',
+      'AryanaSaied_7',
+      'AryanaSaied_6',
+      'AryanaSaied_5',
+      'AryanaSaied_4',
+      'AryanaSaied_3',
+      'AryanaSaied_2',
+      'AryanaSaied_1',
+    ],
         'آریانا سعید',
         'آریانا در سال ۱۹۸۵ در کابل متولد شد. وی در کودکی افغانستان را ترک کرد. آریانا و خانواده‌اش بعد از استقرار در پاکستان به اروپا مهاجرت کردند و بعد از سپری کردن چند سال در سوئیس، در شهر لندن سکونت کردند. علاقهٔ وی به موسیقی از همان کودکی شروع شد. آریانا در دوران تحصیل در زوریخ در کشور سوئیس عضو گروه سرود بود. بعد از فارغ‌التحصیل شدن از رشتهٔ مدیریت تجارت، وی تصمیم گرفت که به عنوان یک هنرمند در عرصهٔ موسیقی فعالیت خود را آغاز کند.',
         'assets/images/singers_images/ArianaSaid.jpg'),
-    Singer([],
-        'فهیم فنا',
-        'بهنام بانی در ۸ سالگی قاری قرآن بود. او پدرش را که نوازنده آکاردئون است، علت گرایش او را به موسیقی می‌داند. وی فهیم فنا یکی از خوانندگان معروف افغان است که در سبک‌های پاپ و کلاسیک فعالیت می‌کند.',
-        'assets/images/singers_images/FahimFana.jpg'),
+    Singer([
+      'TawabArash_5',
+      'TawabArash_2',
+      'TawabArash_6',
+      'TawabArash_4',
+      'TawabArash_1',
+      'TawabArash_3',
+      'TawabArash_7',
+      'TawabArash_8',
+      'TawabArash_9',
+      'TawabArash_10',
+      'TawabArash_11',
+    ],
+        'تواب آرش',
+        ' آرش یکی از خوانندگان مشهور افغانستان است. او به عنوان یکی از هنرمندان پرآوازه و محبوب در موسیقی افغان شناخته می‌شود. تواب آرش با سبک موسیقی خاص خود و اجرای ترانه‌های عاشقانه و ملی‌گرایانه، توانسته است جایگاه ویژه‌ای در میان طرفداران موسیقی افغان کسب کند. صدای خاص و دلنشین او، همراه با ملودی‌های زیبا و احساساتی که در ترانه‌هایش جاری است، او را به یکی از چهره‌های برجسته موسیقی افغان تبدیل کرده است.تواب آرش در طول دوران حرفه‌ای خود، آهنگ‌های زیادی منتشر کرده و در کنسرت‌های مختلف داخلی و بین‌المللی به اجرای برنامه پرداخته است. وی به دلیل علاقه‌اش به فرهنگ و هنر افغانستان، همواره تلاش کرده تا با استفاده از موسیقی، پیام‌هایی از عشق، دوستی، و همبستگی را به مردم منتقل کند.',
+        'assets/images/singers_images/tawabArash.jpg'),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
           PopupMenuButton(
-            icon: Icon(Icons.more_vert,size: 20,color: Colors.white,),
+              icon: Icon(
+                Icons.more_vert,
+                size: 20,
+                color: Colors.white,
+              ),
               onSelected: ChoiceAction,
-              itemBuilder: (BuildContext context){
+              itemBuilder: (BuildContext context) {
                 return MenuItems.choic.map((String choic) {
                   return PopupMenuItem<String>(
                       value: choic,
@@ -99,37 +156,40 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: ListView.builder(
           itemCount: Singers.length,
-          itemBuilder: (context,index){
-        return Card(
-          color: Colors.teal.shade700,
-          child: ListTile(
-            splashColor: Colors.teal,
-            onTap: () {
-              // go to the Singers Details page
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>
-              SingersDetailes(Item: Singers[index])
-              ));
-            },
-            title: Text(
-              '${Singers[index].name}',
-              style: TextStyle(
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.teal.shade700,
+              child: ListTile(
+                splashColor: Colors.teal,
+                onTap: () {
+                  // go to the Singers Details page
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              SingersDetailes(Item: Singers[index])));
+                },
+                title: Text(
+                  '${Singers[index].name}',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('${Singers[index].imageURL}'),
+                ),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
                   color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20),
-            ),
-            leading: CircleAvatar(
-              radius: 30,
-              backgroundImage: AssetImage('${Singers[index].imageURL}'),
-            ),
-            trailing: Icon(
-              Icons.arrow_forward_ios,
-              color: Colors.white,
-            ),
-          ),
-        );
-      }),
+                ),
+              ),
+            );
+          }),
     );
   }
+
   void ChoiceAction(String choic) {
     if (choic == MenuItems.share) {
       // i want to add share  functionality
