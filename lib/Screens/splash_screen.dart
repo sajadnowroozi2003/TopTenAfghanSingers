09e835aa5835f2c12ab01e50d53a825d52c272cc
sajad <sajad.nowroozi5442@gmail.com>
2024-08-24 13:55:35 +0430

    error version of kotlin

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen>
   void initState() {
     // TODO: implement initState
     super.initState();
-    Future.delayed(Duration(seconds: 5), () {
+    Future.delayed(Duration(seconds: 3), () {
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => HomeScreen()));
     });
